0176fde0ca16948573c1ed189f4b5982f711f53c
Tom O'Connell <toconnel@caltech.edu>
2017-08-09 02:27:35 -0700

    Initial thoughts on some python nodes I'd need. Likely won't use.

---
+++ nodes/stimuli.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import rospy
+
+class Stimuli:
+    def __init__(self):
+        # TODO run old code to generate stimuli / save
+        rospy.init_node('stimuli')
+        rospy.Service('get_next_stiminfo', StimInfo, self.send_arduino_stiminfo)
+
+        rospy.spin()
+        
+    def send_arduino_stiminfo(self, req):
+        # are <SrvName>Response types created automatically?
+        # (there is no TestResponse in rosserial_arduino.srv,
+        # see rosserial_arduino examples)
+        
+        return StimInfoRequest()
+        
+        
+
+if __name__ == '__main__':
+    s = Shocks()
