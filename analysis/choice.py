6ea537c6d57709d9a1ef0018a94808165c543f58
Tom O'Connell <toconnel@caltech.edu>
2017-09-14 17:32:50 -0700

    Data centered, patches showing correctly with legend. Still need to check sides aren't flipped in some places and filter bad data.

--- analysis/choice.py
+++ analysis/choice.py
@@ -10,18 +10,18 @@ from os.path import join
 import re
 import pickle
 from stimuli.srv import LoadSequenceRequest
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import seaborn as sns
 # TODO maybe remove this dependency if it means it cant be run easily on windows?
 import rosparam
 
-import ipdb
+make_plots = True
 
-make_plots = False
-
-# was it set_style?
-sns.set()
+# TODO will this work?
+if make_plots:
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
+    import seaborn as sns
+    # was it set_style?
+    sns.set()
 
 # TODO have these parameters save correctly
 left_shock = 6
@@ -124,8 +124,10 @@ for d in map(lambda x: join(path, x), dirs):
         # TODO make it so this generates the (optional) config? notify if doing so
         # TODO TODO exclude trajectories that don't cover enough ground
         data, _ = mta.read_hdf5_file_to_pandas.load_and_preprocess_data(f)
+        '''
         print 'position_x range', data['position_x'].min(), data['position_x'].max()
         print 'position_y range', data['position_y'].min(), data['position_y'].max()
+        '''
         fly2data[curr_fly] = data
         metadata['dir'] = d
         match = re.search('_N([0-9])_', f)
@@ -138,39 +140,67 @@ for d in map(lambda x: join(path, x), dirs):
         fly2meta[curr_fly] = metadata
         curr_fly += 1
 
-# how to use this if not a cmap?
-#cmap = sns.color_palette('husl', len(all_odors), l=0.3, s=0.8, as_cmap=True)
-# TODO have fallback in case as_cmap isn't available
-cmap = sns.color_palette('husl', len(all_odors))
-#sns.hls_palette(len(all_odors), l=0.3, s=0.8)
+if make_plots:
+    cmap = sns.color_palette('husl', len(all_odors))
+    odor2color = dict()
+    for o, c in zip(all_odors, cmap):
+        odor2color[o] = c
 
-odor2color = dict()
-for o, c in zip(all_odors, cmap):
-    odor2color[o] = c
+y_maxes_above_mid = []
+y_mins_below_mid = []
 
-# TODO don't necessarily average across trials (because arena is repositioned wrt camera)
-# TODO just load rois and take middle of that? (assumes you click evenly far away on both sides...)
-y_max = max([data['position_y'].max() for data in fly2data.values()])
-y_min = min([data['position_y'][data['position_y'] > 1].min() for data in fly2data.values() \
-    if np.sum(data['position_y'] > 1) > 0])
-print 'overall y_max', y_max
-print 'overall y_min', y_min
+# TODO assert that we are analyzing with dimension that corresponds to long axis of roi
+
+for m in fly2meta.values():
+    y_vals = map(lambda p: p[1], m['roi_points'])
+    curr_mid = np.mean(y_vals)
+    centered_y_vals = map(lambda y: y - curr_mid, y_vals)
+    y_maxes_above_mid.append(max(centered_y_vals))
+    y_mins_below_mid.append(min(centered_y_vals))
+
+y_max_above_mid = max(y_maxes_above_mid)
+y_min_below_mid = min(y_mins_below_mid)
+
+print 'y_min_below_mid', y_min_below_mid, 'y_max_above_mid', y_max_above_mid
 
 # TODO divide into mch and oct? make a dataframe including all variables (like side, etc)?
 percent_reinforced_pre = []
 percent_reinforced_post = []
 
-crop_to_seconds = 1100
+# TODO why did 1100 not work? isn't sequence 1000 long?
+crop_to_seconds = 1300
 
 # TODO TODO line up images of ROIs, with drawn midline, to check for those errors
-# TODO one y value range for all plots
+# TODO label / group by the reinforced odor?
 
-# TODO legends for odors and shock patch. label / group by the reinforced odor?
+plotting_y_max = None
+plotting_y_min = None
+plotting_x_max = None
+plotting_x_min = None
+
+axes_to_set_limits = []
+
+y_shock_to_odor = 4
+y_beyond_arena = 10
+small_height = 6
+odor_patch_alpha = 0.4
+shock_patch_alpha = 1
+shock_color = 'red'
+
 for fly, data in fly2data.items():
+    meta = fly2meta[fly]
     if make_plots:
+        label_set = set()
+        labels = []
+        handles = []
+
         plt.figure()
-        plt.title(fly2meta[fly]['dir'] + ' ' + str(fly2meta[fly]['n']))
+        plt.title(meta['dir'] + ', ROI ' + str(meta['n']))
         plt.xlabel('Seconds')
+        # TODO no other unit conversion is happening, right?
+        # TODO convert
+        plt.ylabel('Pixels relative to midline')
+
     # TODO what happens to missing data?
     # plotted as zero? not plotted?
     curr_times = (data['time_epoch_secs'] + data['time_epoch_nsecs'] / 1e9).as_matrix()
@@ -178,98 +208,135 @@ for fly, data in fly2data.items():
     #print(curr_times.shape)
     nonzero_times = curr_times.nonzero()[0]
     if nonzero_times.size == 0:
-        print 'SKIPPING FLY ', fly2meta[fly]['n'], 'FROM', fly2meta[fly]['dir']
+        print 'SKIPPING FLY', meta['n'], 'FROM', meta['dir']
         continue
     start = curr_times[nonzero_times].min()
-    print 'START TIME =', start
-    #ipdb.set_trace()
+    #print 'START TIME =', start
+    #print '# of zero entries', curr_times.size - nonzero_times.size
 
     # because for some of these, the tracking ran much longer than the stimulus presentation
     cropped_indices = (curr_times - start) <= crop_to_seconds
     cropped_times = curr_times[cropped_indices]
     # relative to start of experiment
     cropped_rel_times = cropped_times - start
-    if make_plots:
-        plt.plot(cropped_rel_times , data['position_y'][cropped_indices])
 
     # calculate percent time in each odor region
     # TODO make one variable at top which controls which dimension to use as long axis
     # and have this (and elsewhere) depend on that
-    mid = np.mean(map(lambda x: x[1], fly2meta[fly]['roi_points']))
-    print 'using y midline', mid
-    left = data['position_y'][cropped_indices] > mid
-    pretest_indices = np.logical_and(cropped_times > fly2meta[fly]['times']['pretest_start'], \
-        cropped_times < (fly2meta[fly]['times']['pretest_start'] + \
-        fly2meta[fly]['times']['test_duration']))
+    y_mid = np.mean(map(lambda x: x[1], meta['roi_points']))
+    if make_plots:
+        # TODO TODO keep in mind this is probably inverted from image display, since pixels count from top
+        plt.plot(cropped_rel_times , data['position_y'][cropped_indices] - y_mid)
 
-    posttest_indices = np.logical_and(cropped_times > fly2meta[fly]['times']['posttest_start'], \
-        cropped_times < (fly2meta[fly]['times']['posttest_start'] + \
-        fly2meta[fly]['times']['test_duration']))
-    # patch in those indices, also representing which odor is on each side
-    # first argument is lower left corner of rectangle
+    y_max = max(map(lambda x: x[1], meta['roi_points']))
+    y_min = min(map(lambda x: x[1], meta['roi_points']))
+    print 'y_max', y_max, 'y_min', y_min, 'using y midline', y_mid
+    left = data['position_y'][cropped_indices] > y_mid
+    pretest_indices = np.logical_and(cropped_times > meta['times']['pretest_start'], \
+        cropped_times < (meta['times']['pretest_start'] + \
+        meta['times']['test_duration']))
 
-    y_bord = 10
-    y_shock_to_odor = 10
-    large_height = (y_max - y_min) / 2 + y_bord
-    small_height = 20
-    odor_patch_alpha = 0.4
-    shock_patch_alpha = 1
-    shock_color = 'red'
-    # TODO TODO center all plots about mid point. set origin for all to midpoint?
+    posttest_indices = np.logical_and(cropped_times > meta['times']['posttest_start'], \
+        cropped_times < (meta['times']['posttest_start'] + \
+        meta['times']['test_duration']))
 
-    meta = fly2meta[fly]
-    for s in meta['stimuli']:
+    # TODO TODO filter out trajectories that don't move much. just use floris' config file thing?
+
+    large_height = (y_max_above_mid - y_min_below_mid) / 2 + y_beyond_arena
+    
+    for i, s in enumerate(meta['stimuli']):
         width = (s.seq.end - s.seq.start).to_sec()
         for p in s.seq.pins:
-            # TODO fix fact that some x values for patches (and maybe other things?)
-            # are way off (i.e. still in unix epoch time)
+            color = 'b'
+            label = None
             if p is left_shock:
+                label = 'shock'
                 color = shock_color
                 height = small_height
-                y0 = y_min - y_bord - (y_shock_to_odor + small_height)
+                y0 = y_min_below_mid - y_beyond_arena - (y_shock_to_odor + small_height)
                 alpha = shock_patch_alpha
 
             elif p is right_shock:
+                label = 'shock'
                 color = shock_color
                 height = small_height
-                y0 = y_max + y_bord + y_shock_to_odor
+                y0 = y_max_above_mid + y_beyond_arena + y_shock_to_odor
                 alpha = shock_patch_alpha
 
-            # TODO TODO dont use y_min and y_max from across all flies / placements here
             elif p in meta['left_pins2odors']:
-                print 'left odor', meta['left_pins2odors'][p]
-                color = odor2color[meta['left_pins2odors'][p]]
-                # TODO check
-                y0 = y_min - y_bord
+                #print 'left odor', meta['left_pins2odors'][p]
+                if make_plots:
+                    label = meta['left_pins2odors'][p]
+                    color = odor2color[label]
+                y0 = y_min_below_mid - y_beyond_arena
                 height = large_height
                 alpha = odor_patch_alpha
 
             elif p in meta['right_pins2odors']:
-                print 'right odor', meta['right_pins2odors'][p]
-                color = odor2color[meta['right_pins2odors'][p]]
-                # TODO check
-                y0 = mid
+                #print 'right odor', meta['right_pins2odors'][p]
+                if make_plots:
+                    label = meta['right_pins2odors'][p]
+                    color = odor2color[label]
+                # because we define the middle as the new origin
+                y0 = 0
                 height = large_height
                 alpha = odor_patch_alpha
 
             else:
-                print 'skipping pin', p
+                #print 'skipping pin', p
                 continue
 
             # TODO maybe subdivide for shocks? (to reflect actual pulse cycle ~2.5s on/off)
             x0 = s.seq.start.to_sec() - start
-            assert x0 > 0, 'times we want to disply should not be negative. was ' + str(x0)
+            #print 'seq.start.to_sec()', s.seq.start.to_sec(), 'x0', x0, 'width', width
+            if i == 0:
+                # define acceptable mismatch here?
+                if x0 <= 0.0:
+                    print 'WARNING! seems no video was recorded for first ' + \
+                        str(-x0) + ' seconds of first block (pre-training test)'
+
+            else:
+                assert x0 > 0, 'times we want to disply should not be negative. was ' + str(x0)
+
             assert x0 + width < crop_to_seconds, 'last time index of patched region should fall ' + \
-                'within limits of cropped experiment. was ' + str(x0)
-            print 'patching pin', p, 'from', (x0, y0), 'with w=', width, 'and h=', height
+                'within limits of cropped experiment. was ' + str(x0 + width) + ' end=' + \
+                str(crop_to_seconds)
+            #print 'patching pin', p, 'from', (x0, y0), 'with w=', width, 'and h=', height
+
             if make_plots:
                 p = patches.Rectangle((x0, y0), width, height, alpha=alpha, \
                     facecolor=color, edgecolor=color)
                 ax = plt.gca()
                 ax.add_patch(p)
-    
+
+                if (not label is None)  and (not label in label_set):
+                    label_set.add(label)
+                    labels.append(label)
+                    handles.append(p)
+
+                axes_to_set_limits.append(ax)
+
+                # TODO flipped?
+                curr_y_max = y0 + height
+                if plotting_y_max is None or plotting_y_max < curr_y_max:
+                    plotting_y_max = curr_y_max
+
+                curr_y_min = y0
+                if plotting_y_min is None or plotting_y_min > curr_y_min:
+                    plotting_y_min = curr_y_min
+
+                curr_x_max = x0 + width
+                if plotting_x_max is None or plotting_x_max < curr_x_max:
+                    plotting_x_max = curr_x_max
+
+                curr_x_min = x0
+                if plotting_x_min is None or plotting_x_min > curr_x_min:
+                    plotting_x_min = curr_x_min
+
     # patch indications of which sides are shocked, and when
 
+    # TODO TODO check! (is sign effectively switched b/c pixel y origin being on top? plots at least 
+    # may still be shown backwards)
     # TODO include a buffer region that is called in neither direction to make this more robust to 
     # misspecifying the middle of the arena?
     percent_left_pre = np.sum(left[pretest_indices]) / pretest_indices.size
@@ -283,6 +350,25 @@ for fly, data in fly2data.items():
         percent_reinforced_post.append(percent_left_post)
     else:
         percent_reinforced_post.append(1 - percent_left_post)
+
+    if make_plots:
+        plt.legend(handles=handles, labels=labels)
+
+print 'plotting ranges: x', (plotting_x_min, plotting_x_max), 'y', (plotting_y_min, plotting_y_max)
+
+# relative
+y_bord = 0.05
+x_bord = 0.05
+x_range = plotting_x_max - plotting_y_min
+y_range = plotting_y_max - plotting_y_min
+plotting_x_min = plotting_x_min - x_range * x_bord
+plotting_x_max = plotting_x_max + x_range * x_bord
+plotting_y_min = plotting_y_min - y_range * y_bord
+plotting_y_max = plotting_y_max + y_range * y_bord
+
+for ax in axes_to_set_limits:
+    ax.set_xlim([plotting_x_min, plotting_x_max])
+    ax.set_ylim([plotting_y_min, plotting_y_max])
 
 if make_plots:
     plt.figure()
