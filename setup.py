0acc9996829e940e14812eba3cd76d8172a6d373
Tom O'Connell <toconnel@caltech.edu>
2017-09-19 11:03:19 -0700

    Squash me.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='choice',
+    version='0.0.1',
+    author="Tom O'Connell",
+    author_email='toconnel@caltech.edu',
+    scripts=['analysis/choice_analysis'],
+    license='GPLv3',
+    # TODO
+    #description='',
+    #long_description=open('README.md').read(),
+)
