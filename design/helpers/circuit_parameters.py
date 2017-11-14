22aa65361b49c8973fafc6ca5895bac2e743be6a
Tom O'Connell <toconnel@caltech.edu>
2017-11-14 01:12:49 -0800

    Working on calculations. Seems I may need an ADC with more resolution, to better measure the resistance ranges of the flies (assuming the range is potentially smaller than I estimated), or to compress voltages in to the same 10 bit range.

--- design/helpers/circuit_parameters.py
+++ design/helpers/circuit_parameters.py
@@ -31,33 +31,119 @@ vmin_adc = 0
 vmax_adc = 5
 v_per_level = (vmax_adc - vmin_adc) / adc_levels
 
-# TODO do i need to worry about MOSFET Rds(off) (i guess it might be ~= Rfly?)?
-
 # TODO measure
+# maybe w/ oscilloscope? i wasn't really getting readings fast enough w/ a DMM 
+# on 20M resistance range
 min_fly_resistance = 1e6
-max_fly_resistance = 1e10
+# could try 1e7-1e9. previously suspected typical was ~29Mohm, which is 2.9e7
+max_fly_resistance = 1e9 # 1Gohm
 #charge_time_at_min_current = 
 
 # TODO should i use the min resistance to calculate appropriate current limit?
 # or maybe use a current source and measure a current that is just barely not 
 # lethal to most flies?
 max_operation_voltage = 120 # 100?
-typical_operation_voltage = 60
+typ_operation_voltage = 60
 # ?
 min_operation_voltage = 20
 
-
 # TODO decide whether to shut off at this current, or maybe some multiple of it?
 
-# assuming shunt + FET Rds(on) + other resistances are negligible
-# may need to update calculation if they are not
-abs_max_fly_current = max_operation_voltage / min_fly_resistance
+# IRL620 properties
+# at Vgs=5V
+
+# may depend somewhat on Ids, and thus not really be tested at
+# low currents it will see here?
+fet_rds_on = 1
+
+fet_gate_source_leakage = 1e-7  # +/- 100 nA
+# TODO do i need to worry about MOSFET zero gate voltage drain current?
+# FETs do not behave much like resistors when off, so there is no Rds(off)
+# NOTE this value is measured at Vds=200V, though not a linear relationship
+fet_zero_gate_voltage_drain_current = 2.5e-5 # 25 uA
+# end IRL620 properties
+
+# resistance of resistor used to convert the current through the fly to
+# a voltage to be measured
+# should be small enough to not drop a significant fraction of the voltage
+# the fly would be receiving, and large enough to get enough of a voltage
+# to measure
+#shunt_resistance = 1.5e5  # (150K ohms)
+shunt_resistance = 1.5e3
+
+# TODO check assumption that resistances connected to shunt through to ADC
+# are negligible b/c high amp input resitance
+other_series_resistance = shunt_resistance + fet_rds_on
+max_series_resistance = max_fly_resistance + other_series_resistance
+
+min_current = min_operation_voltage / max_series_resistance
+min_shunt_voltage = min_current * shunt_resistance
+
+# still using max series resistance, but with likely operating voltage
+typ_current = typ_operation_voltage / max_series_resistance
+typ_shunt_voltage = typ_current * shunt_resistance
+
+print 'for a shunt (sense) resistance of {:,} ohms...'.format(shunt_resistance)
+
+print '\nassuming min fly resistance of {:,} ohms'.format(min_fly_resistance)
+print ('and with other series resistances totalling {:,} ' + \
+    'ohms').format(other_series_resistance)
+print 'worst case change in fly voltage drop due to other resistances: ' + \
+    '{} %'.format(100 * min_fly_resistance / \
+    (min_fly_resistance + other_series_resistance))
+
+
+print '\nsmallest voltages generated that should be detectable:'
+print 'assuming max fly resistance of {:,} ohms'.format(max_fly_resistance)
+print '  @ {}V'.format(min_operation_voltage)
+print '  {:,}'.format(min_shunt_voltage)
+print '  @ {}V'.format(typ_operation_voltage)
+print '  {:,}'.format(typ_shunt_voltage)
+
+
+max_current = max_operation_voltage / \
+    (min_fly_resistance + other_series_resistance)
+max_shunt_voltage = max_current * shunt_resistance
+
+print '\nlargest voltages generated that should be detectable:'
+print 'assuming min fly resistance of {:,} ohms'.format(min_fly_resistance)
+print '  @ {}V'.format(max_operation_voltage)
+print '  {:,}'.format(max_shunt_voltage)
+
+
+def voltage_dynamic_range(max_detectable, min_detectable):
+    dr = max_detectable / min_detectable
+    # for "field quantities" like voltage, the prefactor of 20 is normal,
+    # rather than the 10 used for "power quantities"
+    dr_db = 20 * np.log10(dr)
+    return dr, dr_db
+
+required_dynamic_range, required_dynamic_range_db = \
+    voltage_dynamic_range(max_shunt_voltage, min_shunt_voltage)
+
+print '\nfrom range of operation voltages and expected range of fly resistances...'
+print 'required dynamic range: {:,} ({}dB)'.format(required_dynamic_range, \
+    required_dynamic_range_db)
+# TODO which is likely easier to implement: invertible compression or external ADC?
+print 'minimum bits of ADC to cover this range w/o compression {}'
 
 # TODO should i make the min voltage at least a few ADC levels?
 # (i.e. what will be precision of upstream circuitry + ADC?)
 # set in a principled way?
-num_levels_for_min = 10
-min_voltage_to_detect = num_levels_for_min * v_per_level
+# TODO TODO
+#num_levels_for_min = 10
+#min_voltage_to_detect = num_levels_for_min * v_per_level
+print '\nminimum detectable voltage difference @ ADC: {}'.format(v_per_level)
+
+# because v_per_level is minimum nonzero voltage
+# (0 would be interpreted as no fly / no shock)
+adc_dr, adc_dr_db = voltage_dynamic_range(vmax_adc, v_per_level)
+print 'assuming the lowest nonzero level is above the noise floor'
+print 'the ADC dynamic range would be: {:,} ({}dB)'.format(adc_dr, \
+    adc_dr_db)
+
+# TODO maybe easiest way around this is just using an external ADC with
+# a higher resolution?
 
 # the goal of a minimum voltage when a fly is receiving a shock
 # plus the maximum resistance we expect a fly to have
@@ -68,6 +154,7 @@ min_voltage_to_detect = num_levels_for_min * v_per_level
 
 # TODO as a function of?
 # TODO include diode # / meaningful name once that subcircuit is finalized
+# TODO matched diodes in same package for symmetric leakage about shunt resistor?
 diode_leak_current = 0.001
 
 
@@ -126,9 +213,16 @@ switch_C_feedthrough = 0.2 * 1e-12
 # TODO make sure analog switch / any voltage follower / anything preceding ADC
 # can all operate in the low
 
+# both Arduino MEGA2560 (ATmega1280) and Arduino Uno (ATmega328/P) have their
+# clock speed set to 16MHz by an onboard crystal oscillator
+# NOTE that the Arduino Due has a higher clock speed
+uc_clock_rate = 1.6e7
+# recommended ADC prescaler. used by analogRead.
+# TODO also the one used on mega?
 # TODO check / get from datasheet
-uc_clock_rate = 1e10
 adc_clock_prescaler = 128
+# TODO that is not for mega though!
+# should be 125kHz ADC clock frequency w/ prescaler of 128
 adc_clock_rate = uc_clock_rate / adc_clock_prescaler
 
 # though it is ~24 on first conversion, for unclear reasons
