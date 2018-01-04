b0f6a57815730920abca77476a782b1ee7d255e6
Tom O'Connell <toconnel@caltech.edu>
2018-01-03 16:40:35 -0800

    Updated test array to (probably) work in '3D mode' on laser cutter, and to better match electrodes.

--- design/helpers/laser_etch_test_array.py
+++ design/helpers/laser_etch_test_array.py
@@ -4,96 +4,77 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-from subprocess import Popen
+import ezdxf
 
-# TODO fix
-# options are 'openscad' or 'freecad', but 'openscad' can't do most of the work
-# 'ezdxf'
-# because it doesn't seem to be able to export lines with color (solid uses scad)
-intermediate = 'ezdxf'
-
-if intermediate == 'openscad':
-    import solid as s
-
-elif intermediate == 'ezdxf':
-    import ezdxf
-    dxf = ezdxf.new('R2013')
-    msp = dxf.modelspace()
+dxf = ezdxf.new('R2013')
+msp = dxf.modelspace()
 
 ###############################################################################
-radius_mm = 1.25
+# for concentric squares
+inner_width_mm = 0.4
+border_width_mm = 0.6
+between_mm = 0.4
 
-# might want to make sure this is large enough
-# thoug we would want to etch at this pitch or finer
-between_circles_mm = 1
+#just_black = True
 
-n_colors = 3
+#n_colors = 3
+# none of the drawing programs i use (inkscape / coreldraw) seem to recognize
+# the transparency DXF attribute
 n_intensities = 16
-
-segments_per_circle = 32
+max_intensity = 255
 ###############################################################################
 
-assert n_colors <= 3, 'only export of 3 colors supported for now (RGB)'
-
-# TODO how to programmatically control both speed and power?
+# TODO how to programmatically control both speed and power? (looks like i can't)
+# resolution?
 # pick n colors = n speeds and vary power?
 # is it even worth manipulating them independently?
 # alternatives?
-# TODO can solid scad_render a list or what? some kind of list type?
 
-if intermediate == 'openscad':
-    # generate the solidpython representation of the array we want
-    circles = []
+# length of unit making up one side of concentric square shape
+unit_length = border_width_mm + inner_width_mm
+def side_rect(top_left, long_vertical=True):
+    def off(p, x, y):
+        return (p[0] + x, p[1] + y)
 
-for i in range(n_colors):
-    c = [0, 0, 0]
-    c[i] = 1
-    x_offset = (2*radius_mm + between_circles_mm) * i
+    if long_vertical:
+        return [top_left, off(top_left, border_width_mm, 0), \
+            off(top_left, 0, unit_length), \
+            off(top_left, border_width_mm, unit_length)]
+    else:
+        return [top_left, off(top_left, 0, border_width_mm), \
+            off(top_left, unit_length, 0), \
+            off(top_left, unit_length, border_width_mm)]
 
-    # TODO should i change the intensity w/ alpha or color level?
+base_coords = [side_rect((0, 0)), side_rect((unit_length, border_width_mm)), \
+    side_rect((border_width_mm, 0), long_vertical=False), \
+    side_rect((0, unit_length), long_vertical=False)]
+print(base_coords)
+
+# TODO provide option to specify x and y dims of array
+for i in range(1):
+    x_offset = (inner_width_mm + 2*border_width_mm + between_mm) * i
     for j in range(n_intensities):
-        y_offset = (2*radius_mm + between_circles_mm) * j
-        alpha = (j + 1) / n_intensities
+        y_offset = (inner_width_mm + 2*border_width_mm + between_mm) * j
+        level = int(round(max_intensity * (j + 1) / n_intensities))
 
-        # color argument: 0=r, 1=g, 2=b, 3=alpha
-        if intermediate == 'openscad':
-            # TODO move shared calculations out of if
-            circles.append(s.color(c + [alpha])(\
-                s.translate([x_offset, y_offset, 0])(s.circle(radius_mm))))
+        # 1=r,3=g,5=b(,2=y,4=c,6=m)
+        autocad_color_index = (2*i) + 1
+        
+        for shape_coords in base_coords:
+            offset_coords = [(c[0] + x_offset, c[1] + y_offset) for c in shape_coords]
+            # TODO ok if points is a list of tuples?
+            s = msp.add_solid(offset_coords)
+            print(offset_coords)
+            # TODO is color thing working? would it work any better with truecolor?
+            # just find a function to convert rgb to autocad color index 
+            # and use only r12 features?
+            s.rgb = (level, level, level)
+            print(s.rgb)
 
-        elif intermediate == 'ezdxf':
-            # 1=r,3=g,5=b(,2=y,4=c,6=m)
-            autocad_color_index = (2*i) + 1
-            msp.add_circle(center=(x_offset, y_offset), radius=radius_mm, \
-                dxfattribs={'color': autocad_color_index, \
-                'transparency': int(round(alpha * 255))})
+            for p in s:
+                print(p)
 
 
 prefix = '.'.join(os.path.split(__file__)[1].split('.')[:-1])
-
-
-if intermediate == 'openscad':
-    # convert this to OpenSCAD format
-    outfile = prefix + '.scad'
-    with open(outfile, 'w') as f:
-        for i, c in enumerate(circles):
-            # TODO would it make sense to add iterable rendering for solidpython?
-            # (i.e. does it make sense to have a set of top level 
-            #  objects(?) in openscad?)
-            if i == 0:
-                scad = s.scad_render(c, \
-                    file_header='$fn = {};'.format(segments_per_circle))
-            else:
-                scad = s.scad_render(c)
-            f.write(scad)
-
-    # TODO need to specify units somewhere?
-
-    # call openscad to turn the .scad file into a dxf
-    # TODO maybe just insert the special variable(s) here? (e.g. $fn)
-    # TODO don't hang open
-    Popen(['openscad', '-o', prefix + '.dxf', outfile])
-
-elif intermediate == 'ezdxf':
-    dxf.saveas(prefix + '.dxf')
+dxf.saveas(prefix + '.dxf')
 
